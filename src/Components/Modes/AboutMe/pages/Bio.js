7193d0035a904a034923b9d408a5bdce310018d2
Dynamic Bineuro <dynamic.bineuro@gmail.com>
2025-06-06 01:19:25 -0700

    Finally managed to connect to firebase

--- src/Components/Modes/AboutMe/pages/Bio.js
+++ src/Components/Modes/AboutMe/pages/Bio.js
@@ -6,7 +6,7 @@ export default function Bio(){
                 His main interests are game development, basketball and <i>(of course)</i> programming. Ahmed carries a diverse set of expereinces from many different fields <i>(like human computer interaction and data analysis)</i> which
                  allows him to quickly be able to learn and pick up new and different topics and concepts relatively quickly. Aside of software development, Ahmed has also gained expereince 
                 with embedded systems <i>(specially with the <a href="https://www.espressif.com/">ESP</a> and <a href="https://www.arduino.cc/">Arduino</a> platforms)</i> as well as visual design <i>(through tools like <a href="https://www.figma.com/sites/">Figma
-                </a>,<a href="https://www.adobe.com/"> Adobe Illustrator</a>, and<a href="https://www.adobe.com/">Adobe XD</a>)</i>.</p>
+                </a>,<a href="https://www.adobe.com/"> Adobe Illustrator</a>, and<a href="https://www.adobe.com/">Adobe XD</a>)</i> .</p>
 
             <p className='FeedContent'>
                 <b>Want to know more?</b> Navigate through the diffrerent pages to learn about my skills, projects and contact information!
--- src/Components/Modes/Projects/Projects.js
+++ src/Components/Modes/Projects/Projects.js
@@ -1,6 +1,6 @@
 import { RadioButtonList } from "../../Reusable/GlowingGUI.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+import { getFirestore,collection,query,getDoc,getDocs, doc,DocumentReference ,where} from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 
 import MiniNav from "../AboutMe/MiniNav.js";
 /**
@@ -12,8 +12,66 @@ import MiniNav from "../AboutMe/MiniNav.js";
  *      - Image
  *      - Tags (An array of one word descriptors)
  */
-const app=initializeApp();
-console.log(app);
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAzSjCbYwpY-N55FzhEOF6qqw_CuEdiZl0",
+    authDomain: "dynamicbineurowebsite.firebaseapp.com",
+    databaseURL: "https://dynamicbineurowebsite-default-rtdb.firebaseio.com",
+    projectId: "dynamicbineurowebsite",
+    storageBucket: "dynamicbineurowebsite.firebasestorage.app",
+    messagingSenderId: "788647486588",
+    appId: "1:788647486588:web:af786d31486d70b5f095f9",
+    measurementId: "G-QHYPD6QQGT"
+  };
+    
+const app=initializeApp(firebaseConfig);
+const db=getFirestore(app);
+/**
+ * //How to read a doc
+        //Get a reference for the query
+        const docRef=doc(db,"Projects","0");
+        //Use the reference to request the data
+        const docSnap= await getDoc(docRef);
+        if(docSnap.exists()){
+            console.log(docSnap.data());
+        }
+        else{
+            console.log("Not successful");
+        }
+*/
+
+async function getProjects() {
+    //How to read a collection
+    var projects=[];
+    var tags={};
+    const q=query(collection(db,"Projects"));
+    const qSnapshot=await getDocs(q);
+    qSnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        let Tags= [];
+        doc.data().Tags.forEach((tag,key)=>{Tags.push(key)});
+        projects.push({
+            id: doc.id,
+            Name: doc.data().Name,
+            Tags:Tags
+        });
+    });
+    const q2=query(collection(db,"Tags"));
+    const q2Snapshot=await getDocs(q2);
+    q2Snapshot.forEach((doc)=>{
+        tags[doc.id]=doc.data().Name;
+    });
+    return( 
+    {
+        Projects:projects,
+        Tags:tags
+    });
+}
+
 export function Projects(){
+    //Use useEffect to handle async operations
     return <><h1>Projects</h1></>;
 }
