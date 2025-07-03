30c84c065042a6de5408295a907fc7ddcf34ba6b
Ahmed Abdalla <dynamic.bineuro@gmail.com>
2025-07-03 11:34:50 -0700

    started working on carousel

---
+++ src/Components/Reusable/MediaCarousel.js
@@ -0,0 +1,15 @@
+import MediaContainer from "./MediaContainer"
+import { Tag } from "./Tag"
+import { useState } from "react";
+export default function MediaCarousel({Projects,Tags}){
+    const [index,setIndex] = useState((Projects.length()/2)-1);
+    let start=((index-1)<0)?(0):(index-1);
+    let end;
+    if(index ===0)
+        end=1;
+    else
+        end=index+1;
+    const displayProjects=Projects.subarray(start,end);
+    
+    return (<></>)
+}
