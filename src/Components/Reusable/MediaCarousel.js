ca76d23065d81a9cc7fe5a369656420d460020c2
Ahmed Abdalla <dynamic.bineuro@gmail.com>
2025-08-18 19:24:25 -0700

    Finished the project carousel

--- src/Components/Reusable/MediaCarousel.js
+++ src/Components/Reusable/MediaCarousel.js
@@ -1,25 +1,47 @@
 import MediaContainer from "./MediaContainer.js"
 import './MediaCarousel.css'
 export default function MediaCarousel({Projects,Tags}){
-    // const [index,setIndex] = useState((Projects.length()/2)-1);
-    // let start=((index-1)<0)?(0):(index-1);
-    // let end;
-    // if(index ===0)
-    //     end=1;
-    // else
-    //     end=index+1;
-    // const displayProjects=Projects.subarray(start,end);
+    var index=0;
     var projectList=[];
     if(Projects!==undefined&&Tags!==undefined)
                 projectList=Projects.map((project,index)=><MediaContainer key={index} Tags={project.Tags.map(tag=>(Tags[tag]!==undefined)?Tags[tag].Name:[])} Name={project.Name} URL={project.URL} Thumbnail={project.Thumbnail} />);
+    
+    //direciton is a value either -1 for left or 1 for right which will set the increment
+    const scrollProject=(event)=>{
+        let classList=event.target.classList;
+        let direction=0
+        classList.forEach((className)=>{
+            if(className==="Left")
+                direction=-1;
+            if(className==="Right")
+                direction=1;
+        })
+        let projectDisplay=document.getElementById("ProjectDisplay");
+        if(!projectDisplay)
+            return;                
+        let width=projectDisplay.scrollWidth;
+        if(projectList.length===0)
+            return
+        let increment=(width/projectList.length);
+        let newIndex=index+direction;
+        if(newIndex>=projectList.length)
+            newIndex=0;
+        else if(newIndex<0)
+            newIndex=projectList.length-1;
+        index=newIndex;
+        let dist=increment*newIndex;
+        console.log(dist);
+        projectDisplay.scroll({left:dist});
+        console.log(index);
+    };
     return (<>
     <div className="MediaCarouselWrapper">
         <div className="MediaCarousel">
-            <button className="CarouselButton Left">{'<'}</button>
-                <div className="ProjectListWrapper">
+            <button className="CarouselButton Left" onClick={scrollProject}>{'<'}</button>
+                <div id="ProjectDisplay" className="ProjectListWrapper">
                     {(projectList.length!==0)?projectList:<em className="Error">The relevant projects have not been added yet!</em>}
                 </div>
-            <button className="CarouselButton Right">{'>'}</button>
+            <button className="CarouselButton Right" onClick={scrollProject}>{'>'}</button>
         </div>
     </div>
     </>)
