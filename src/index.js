8cdeefb385452c5060da7487a3b3c255302e4a71
Dynamic Bineuro <dynamic.bineuro@gmail.com>
2025-08-21 19:35:22 -0700

    Removed strict mode part 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Components/App.js';
-import { StrictMode } from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
